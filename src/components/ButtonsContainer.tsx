b76c7148816076ac5bf107a8f85d1b0b8d1c1e1a
Hector <torrezhectorb@gmail.com>
2023-07-06 18:01:08 -0600

    feature/ add functions

--- src/components/ButtonsContainer.tsx
+++ src/components/ButtonsContainer.tsx
@@ -1,21 +1,24 @@
+import { useShoppingCartProvider } from '../hooks/useShoppingCartProvider'
 import { CardButton } from './CardButton'
 interface Props {
   quantity: number
+  id: number
 }
 
-export const ButtonsContainer: React.FC<Props> = ({ quantity }: Props) => {
+export const ButtonsContainer: React.FC<Props> = ({ quantity, id }: Props) => {
+  const { increseCartQuantity, decreaseCartQuantity } = useShoppingCartProvider()
   return (
     <>
      {
                 quantity === 0
                   ? (
-                    <CardButton text='+ Add To Cart'/>
+                    <CardButton click={() => increseCartQuantity(id)} text='+ Add To Cart'/>
                     )
                   : (
                     <section className='flex justify-center gap-5 items-center' >
-                    <CardButton text='-'/>
+                    <CardButton click={() => decreaseCartQuantity(id)} text='-'/>
                     {quantity} in Cart
-                    <CardButton text='+'/>
+                    <CardButton click={() => increseCartQuantity(id)} text='+'/>
 
                     </section>
                     )
