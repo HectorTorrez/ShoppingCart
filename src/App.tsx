d2863eb54c8ce0de24470840d11b591d598051fb
Hector <torrezhectorb@gmail.com>
2023-07-07 20:37:17 -0600

    delete a change provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Products } from './pages/Products'
-import { ProductsProvider } from './context/ProductsProvider'
-import { ShoppingCartProvider } from './context/ShoppingCartProvider'
 
 export const App: React.FC = () => {
   return (
-      <ProductsProvider>
-        <ShoppingCartProvider>
+
          <Routes>
           <Route path='/' element={<Products/>} />
           </Routes>
-        </ShoppingCartProvider>
-      </ProductsProvider>
+
   )
 }
