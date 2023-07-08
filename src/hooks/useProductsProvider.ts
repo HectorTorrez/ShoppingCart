03764a5fc9eb5e4e508a5e8ed6c0be5e5bd48de0
Hector <torrezhectorb@gmail.com>
2023-07-07 20:35:56 -0600

    Delete context

--- src/hooks/useProductsProvider.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-import { ProductsContext } from '../context/ProductsProvider'
-
-export const useProductsProvider = (): any => {
-  // if (!ProductsContext) throw new Error('There is not products provider')
-  return useContext(ProductsContext)
-}
