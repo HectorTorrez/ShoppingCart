fa614338143eaddbeafa765a92ef5a7f7e868243
Hector <torrezhectorb@gmail.com>
2023-07-12 15:47:44 -0600

    delete customHook

--- src/hooks/useShoppingCartProvider.ts
+++
@@ -1,6 +0,0 @@
-import { ShoppingCartContext } from '../context/ShoppingCartProvider'
-import { useContext } from 'react'
-
-export const useShoppingCartProvider = (): any => {
-  return useContext(ShoppingCartContext)
-}
