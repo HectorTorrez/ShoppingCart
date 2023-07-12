2656c4b93692de99eaeab7d082b32f7366087356
Hector <torrezhectorb@gmail.com>
2023-07-11 20:21:50 -0600

    feature/ total

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,10 +1,11 @@
+import { formatCurrency } from '../utilities/FormatCurrency'
 import { StoreItem } from './StoreItem'
 
 export const ShoppingCart = ({ cartItems, closeCart, dataProducts }) => {
   return (
-    <section className="flex p-5 gap-10 flex-col fixed top-0 right-0 h-screen lg:w-2/5 bg-white overflow-auto ">
+    <section className="flex p-5 gap-10 flex-col fixed top-0 right-0 h-full w-full lg:w-2/5 bg-white overflow-auto  ">
     <header className="flex justify-between items-center">
-      <h2>Store</h2>
+      <h2 className='font-bold text-xl'>Store</h2>
       <button onClick={closeCart} className="text-2xl font-bold">&times;</button>
     </header>
 
@@ -13,6 +14,16 @@ export const ShoppingCart = ({ cartItems, closeCart, dataProducts }) => {
               return <StoreItem key={product.id} {...product} dataProducts={dataProducts} />
             })
     }
+
+    <section className='flex justify-between font-bold text-3xl items-center'>
+      <p className='font-bold text-5xl'>Total</p>
+      {formatCurrency(
+        cartItems.reduce((total, cartItem) => {
+          const item = dataProducts.find(i => i.id === cartItem.id)
+          return total + (item?.price || 0) * cartItem.quantity
+        }, 0)
+      )}
+    </section>
     </section>
   )
 }
